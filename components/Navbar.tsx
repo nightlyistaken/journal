ff27a1f2e5534443726dc8116d3f23a2a0369635
Nightly <dhairysrivastava5@gmail.com>
2022-12-11 14:50:43 +0530

    oops

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,8 +4,8 @@ export function Navbar() {
       <nav class="flex justify-between text-black w-screen">
         <div class="px-5 xl:px-10 py-6 flex w-full items-center">
           <a class="text-3xl font-bold font-heading" href="#">
-            <span class="bg-gradient-to-br text-transparent bg-clip-text from-indigo-900 via-pink-800 to-indigo-800">
-              Journal
+            <span class="bg-gradient-to-br text-transparent bg-clip-text from-gray-900  to-indigo-800">
+              Endless
             </span>
           </a>
 
